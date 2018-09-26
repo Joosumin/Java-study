3055532b43df26c74c0f227915e03d0e9f557984
sumin <cotack@naver.com>
2018-09-26 23:59:43 +0900

    java day1

---
+++ day1/src/day1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class day1 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int input = scan.nextInt();
+		for(int i=1;i<=input ; i++) {
+			System.out.println(i);
+			
+		}
+		
+		
+
+	}
+
+}
